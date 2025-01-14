e4193c180f065b1e56d9a7a0b79abed5778144a7
HadayaRahman-SF4673 <hadayarahman.m@syncfusion.com>
2025-01-14 20:03:47 +0530

    Added for Camera Page

--- SmartHome/SmartHome/SmartHome/Views/Bathroom.xaml.cs
+++ SmartHome/SmartHome/SmartHome/Views/Bathroom.xaml.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-
 namespace SmartHome;
 
 public partial class Bathroom : ContentView
@@ -7,26 +5,24 @@ public partial class Bathroom : ContentView
     public Bathroom()
     {
         InitializeComponent();
-        if ((bool)heaterSwitch.IsOn!)
-        {
-            linearGauge.IsEnabled = true;
-            radialGauge.IsEnabled = true;
-            pointer.Fill = Color.FromArgb("#77a0d3");
-            pointerBackground.Fill = Color.FromArgb("#C9FCFD"); ;
-        }
-        else
-        {
-            linearGauge.IsEnabled = false;
-            radialGauge.IsEnabled = false;
-            pointer.Fill = Colors.DarkGray;
-            pointerBackground.Fill = Colors.LightGray;
-        }
+
+        // Sets Default Appearence for Bathroom Page since the switch is default value is off here.
+        linearGauge.IsEnabled = false;
+        radialGauge.IsEnabled = false;
+        pointer.Fill = Colors.DarkGray;
+        pointerBackground.Fill = Colors.LightGray;
     }
 
+    /// <summary>
+    /// Used for changing appearence of the page according to the switch.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void heaterSwitch_StateChanged(object sender, Syncfusion.Maui.Buttons.SwitchStateChangedEventArgs e)
     {
         if ((bool)heaterSwitch.IsOn!)
         {
+            // Sets the both gauges to enable state and sets expected colors for the linear gauge.
             linearGauge.IsEnabled = true;
             radialGauge.IsEnabled = true;
             pointer.Fill = Color.FromArgb("#77a0d3");
@@ -34,6 +30,7 @@ public partial class Bathroom : ContentView
         }
         else
         {
+            // Sets the both gauges to disable state and sets gray color for the linear gauge.
             linearGauge.IsEnabled = false;
             radialGauge.IsEnabled = false;
             pointer.Fill = Colors.DarkGray;
